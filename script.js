de70919237cbcd99c1f5b6a454501d58fbb5271d
Avalon <avalonslake@gmail.com>
2025-07-01 10:41:47 -0600

    final commit

--- script.js
+++ script.js
@@ -9,11 +9,7 @@ function addProduct() {
   // You will need to create a div using javascript,
   // Use this site to help you understand how: https://www.w3schools.com/jsref/met_document_createelement.asp
   let output = document.getElementById("output");
-  output.innerHTML = `${input}`;
-
-  if (input === "") {
-    alert("You can't have an empty input, please submit something");
-  }
+  const list = document.createElement("div");
 
   // Once you create create a div,
   // you will need to get the output using the same method you did for the input
@@ -27,6 +23,11 @@ function addProduct() {
   // If you do not know the alert method, look it up on google using the w3schools,
   // do not forget to include a return false at the end to not reload your webpage.
 
+  if (input === "") {
+    alert("You can't have an empty input, please input something");
+    return false;
+  }
+
   // Assignment 2:
   // You need to put the value of the input into an array to call later.
   // That means you will need to assign your input using brackets around it
@@ -38,12 +39,12 @@ function addProduct() {
   // that makes a single object {} with the product variable you made above.
   // Please look up how to use push on an array here: https://www.w3schools.com/jsref/jsref_push.asp
   // use console.log and pass in the product array to check if you have it setup correctly.
-  // console.log(productArray)
 
   // Assignment 3:
-  // Hint: Use a loop(look up the javascript loop on w3schools.) on your product array and set the max length of the loop to the array using
+  // Hint: Use a loop(look up the javascript loop on w3schools.) on your product array and
+  // set the max length of the loop to the array using
   // dot notation (array.length) and then you do the following:
-  //
+
   // set the input to empty, similar how you would check in the if statement previously,
   // just get document.getelementbyid then set the .value to an empty string.
 
@@ -55,6 +56,13 @@ function addProduct() {
 
   // finally use the appendChild method from the output variable you had from earlier.
   // output.appendChild(yourdivnamehere)
+
+  for (let i = 0; i < productArray.length; i++) {
+    document.getElementById("product").value = "";
+    output.style.display = "block";
+    list.textContent = `${productArray[i].input}`;
+    output.appendChild(list);
+  }
 
   // this line is to prevent the webpage from reloading. DO NOT MODIFY IT.
   return false;
